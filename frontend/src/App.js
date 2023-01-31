bef4d113c5b61abff17be295a2843aba7edf6c27
Code-hyunwoo <chichyun92@naver.com>
2023-01-31 10:43:23 +0900

    Feat: CRA, CSS Setting

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import intro from './assets/image/intro.png'
+
+function App() {
+  return (
+    <div className="App">
+      TokGit
+      <img class="intro" src={intro} alt='intro'/>
+
+    </div>
+  );
+}
+
+export default App;

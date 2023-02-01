57b3ed3b7ce11ff697fdee736b8c1bec50b7dc04
Code-hyunwoo <chichyun92@naver.com>
2023-02-02 01:50:49 +0900

    Feat: Button, Snow, Font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import intro from './assets/image/intro.png'
 import Title from './components/home/Title';
-import BG from './assets/image/rabbit.png'
-import BG2 from './assets/image/rabbit2.png'
 import Snow from './components/home/Snow';
+import Button from './components/btn/Button';
 
 function App() {
   return (
@@ -11,9 +10,8 @@ function App() {
       <Snow />
       <div className="App">
         <Title />
-        <img className="BG" src={BG} alt="BG"/> 
-        <img className="BG2" src={BG2} alt="BG"/> 
         <img class="intro" src={intro} alt='intro'/>
+        <Button text="Github 계정으로 시작하기"/>
 
       </div>
     </>

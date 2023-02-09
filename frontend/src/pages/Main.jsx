ca3390cd76ad2d16cc1658828d309bf20c3945e8
Code-hyunwoo <chichyun92@naver.com>
2023-02-10 02:18:38 +0900

    Feat: Main, Settings

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import Nav from "../components/nav/Nav";
 import { motion } from "framer-motion";
-
+import styles from './Main.module.css'
+import Pet from '../assets/image/rabbit2.png'
 
 export default function Main() {
   
@@ -11,8 +12,23 @@ export default function Main() {
     <motion.div 
     initial={{opacity: 0}}
     animate={{opacity: 1}}
-    exit={{opacity: 0}}>
-        유저 기본 화면
+    exit={{opacity: 0}} className={styles.maindiv} >
+        <div className={styles.main_left}>
+          <img className={styles.Pet} src={Pet} alt="Pet" />
+          <div> 캐릭터 닉네임</div>
+          <div> 총 커밋 수: 100</div>
+          <div className={styles.level}> Level 99</div>
+        </div>
+
+        <div className={styles.main_right}>
+          <div> 오늘의 커밋 횟수</div>
+          <div> 커밋 bar 그래프</div>
+
+          <div> 최근 커밋 내역 </div>
+          <div>  커밋 내역 </div>
+          <div>  오늘의 한 마디 </div>
+
+        </div>
     </motion.div>
     
     </>
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,6 +1,7 @@
 import Nav from "../components/nav/Nav";
 import { motion } from "framer-motion";
-
+import Button from "../components/btn/Button";
+import styles from "./Rank.module.css"
 
 export default function Settings() {
   
@@ -11,8 +12,10 @@ export default function Settings() {
     <motion.div 
     initial={{opacity: 0}}
     animate={{opacity: 1}}
-    exit={{opacity: 0}}>
-        Settings
+    exit={{opacity: 0}} className={styles.settings_div}>
+        <Button text="닉네임 변경하기"/>
+        <Button text="만든 사람들"/>
+        <Button text="로그아웃"/>
     </motion.div>
     </>
   )
